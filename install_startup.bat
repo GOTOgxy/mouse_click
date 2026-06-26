5823a3a0a66eecdc0541c8fd98b6930af8ce8150
gxy <gotogxy@outlook.com>
2026-06-26 09:32:39 +0800

    rename 启动.bat to mouse_click.bat

--- install_startup.bat
+++ install_startup.bat
@@ -6,7 +6,7 @@ cd /d "%~dp0"
 set "STARTUP_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "TARGET=%STARTUP_DIR%\mouse_remapper.bat"
 
-copy /y "%~dp0启动.bat" "%TARGET%" >nul
+copy /y "%~dp0mouse_click.bat" "%TARGET%" >nul
 
 if exist "%TARGET%" (
   echo Startup install complete.
